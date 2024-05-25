a5011ab7fb4963e7d255193551679b9d1e71da87
RaelzeraXD <raelsf.100@gmail.com>
2024-05-25 16:44:29 -0300

    testejubileu2

--- cloudformation/main.go
+++ cloudformation/main.go
@@ -7,7 +7,7 @@ import (
 
 func main(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "jubileu da massa")
+		fmt.Fprintf(w, "jubileu brabao")
 	})
 	http.ListenAndServe(":80", nil)
 }
