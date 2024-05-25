435a1af05d04f93a080dc7a01c22d1e83a5e4cdf
RaelzeraXD <raelsf.100@gmail.com>
2024-05-25 13:08:59 -0300

    testersync2

--- cloudformation/main.go
+++ cloudformation/main.go
@@ -7,7 +7,7 @@ import (
 
 func main(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Oi, meu chapa")
+		fmt.Fprintf(w, "testersync")
 	})
 	http.ListenAndServe(":80", nil)
 }
