36996c05ba239a213a7734ef5e93b7be89c46398
RaelzeraXD <raelsf.100@gmail.com>
2024-05-25 18:59:41 -0300

    edtech

--- cloudformation/main.go
+++ cloudformation/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "lasanha")
+		fmt.Fprintf(w, "olá,edtech")
 	})
 	http.ListenAndServe(":80", nil)
 }
