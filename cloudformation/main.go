45d10e74f0eb40336d87c13710e58fccc63a5932
RaelzeraXD <raelsf.100@gmail.com>
2024-05-25 19:26:14 -0300

    sem pkill

--- cloudformation/main.go
+++ cloudformation/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "teste pkill")
+		fmt.Fprintf(w, "teste sem kill")
 	})
 	http.ListenAndServe(":80", nil)
 }
