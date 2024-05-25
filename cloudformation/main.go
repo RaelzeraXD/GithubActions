b4054b9b9380915789706e24fc7e1707e229d719
RaelzeraXD <raelsf.100@gmail.com>
2024-05-25 19:48:52 -0300

    teste final

--- cloudformation/main.go
+++ cloudformation/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "teste com comando antigo")
+		fmt.Fprintf(w, "teste final")
 	})
 	http.ListenAndServe(":80", nil)
 }
