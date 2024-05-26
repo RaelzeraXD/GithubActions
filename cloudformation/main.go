9e4c90098d7258065a6093f310a200bacc909c84
RaelzeraXD <raelsf.100@gmail.com>
2024-05-26 04:49:27 -0300

    cloudformation1000

--- cloudformation/main.go
+++ cloudformation/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "teste cloudformation200")
+		fmt.Fprintf(w, "teste1000")
 	})
 	http.ListenAndServe(":80", nil)
 }
