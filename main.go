99595078f2b196252e817ab70ce8d3f9bccaa196
RaelzeraXD <raelsf.100@gmail.com>
2024-05-26 16:54:16 -0300

    deusémais3.0

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Olá, Edtech")
+		fmt.Fprintf(w, "Olá,Edtech")
 	})
 	http.ListenAndServe(":80", nil)
 }
